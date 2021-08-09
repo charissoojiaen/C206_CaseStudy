dfa8247841c6fd14f550e157d7c0649b322ae854
20025077 <20025077@DESKTOP-11R6C80>
2021-08-09 21:21:34 +0800

    update cca category new class

--- C206_CaseStudy/src/Category.java
+++ C206_CaseStudy/src/Category.java
@@ -1,4 +1,26 @@
 
 public class Category {
+	
+	private String name;
+	private String description;
+	
+	public Category(String name, String description) {
+		this.name = name;
+		this.description = description;
+		
+	}
+	
+	public String getCategoryName() {
+		return name;
+	}
+	
+	public String getCategoryDesc() {
+		return description;
+	}
+	
+	public String toString(){
+		String output = "";
+		return output;
+	}
 
 }
