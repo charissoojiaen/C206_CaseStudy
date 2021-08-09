8424f023d083dc485d86a6a8652a14692513cd0e
20025077 <20025077@DESKTOP-11R6C80>
2021-08-09 21:31:18 +0800

    new Student class

---
+++ C206_CaseStudy/src/Student.java
@@ -0,0 +1,38 @@
+
+public class Student {
+
+	private String name;
+	private int id;
+	private String grade;
+	private String classes;
+	private String teacher;
+
+	public Student(String name, int id, String grade, String classes, String teacher) {
+		this.name = name;
+		this.id = id;
+		this.grade = grade;
+		this.classes = classes;
+		this.teacher = teacher;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public String getClasses() {
+		return classes;
+	}
+
+	public String getTeacher() {
+		return teacher;
+	}
+
+}
