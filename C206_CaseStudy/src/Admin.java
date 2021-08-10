e55e25524237c3b7383af4e30b5c5d0ec847fdc7
gilli <gilli@LAPTOP-QD4E7I4V>
2021-08-10 20:29:05 +0800

    Admin

---
+++ C206_CaseStudy/src/Admin.java
@@ -0,0 +1,39 @@
+/**
+ * I declare that this code was written by me. 
+ * I will not copy or allow others to copy my code
+ * I understand that copying code is considered plagiarism.
+ *
+ *Student Name: Gillian Koh
+ *Student ID: 20012033
+ *Class: W65J
+ *Date/Time Last modified: 
+ */
+
+/**
+ * @author gilli
+ *
+ */
+public class Admin {
+	private String name;
+	private String id;
+	private String password;
+	
+	public Admin(String name, String id, String password) {
+		this.name = name;
+		this.id = id;
+		this.password = password;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+}
