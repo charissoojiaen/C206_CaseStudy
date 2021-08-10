df15d51ae0bd6492b990327a21b6560c1cc21648
20025077 <20025077@DESKTOP-11R6C80>
2021-08-10 21:42:13 +0800

    new Parent class

--- C206_CaseStudy/src/Parent.java
+++ C206_CaseStudy/src/Parent.java
@@ -1,4 +1,38 @@
 
 public class Parent {
+	private String name;
+    private int phoneNumber;
+    private int regID;
+    
+    public Parent(String name, int phoneNumber, int regID) {
+      this.name = name;
+      this.phoneNumber = phoneNumber;
+      this.regID = regID;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+  public int getPhoneNumber() {
+      return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+      this.phoneNumber = phoneNumber;
+    }
+
+
+    public int getRegID() {
+    return regID;
+  }
+
+  public void setRegID(int regID) {
+    this.regID = regID;
+  }
 
 }
