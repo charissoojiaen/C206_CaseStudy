c875d56f2787470d2a3353090b9aca865db76477
20025077 <20025077@DESKTOP-11R6C80>
2021-08-11 14:02:40 +0800

    new ParentRegister and RegisteredStudents classes

---
+++ C206_CaseStudy/src/ParentRegister.java
@@ -0,0 +1,17 @@
+
+public class ParentRegister {
+	
+		private String studentId;
+		private String regiId;
+		public ParentRegister(String studentId, String regiId) {
+		this.studentId = studentId;
+		this.regiId = regiId;
+		}
+		public String getStudentId() {
+		return studentId;
+		}
+		public String getRegiId() {
+		return regiId;
+		}
+
+}
---
+++ C206_CaseStudy/src/RegisteredStudents.java
@@ -0,0 +1,22 @@
+
+public class RegisteredStudents {
+	
+		private int studentId;
+		private String name;
+		private String cca;
+		public RegisteredStudents(int studentId, String name, String cca) {
+		this.studentId = studentId;
+		this.name = name;
+		this.cca = cca;
+		}
+		public int getRegiStudentId() {
+		return studentId;
+		}
+		public String getRegiName() {
+		return name;
+		}
+		public String getRegiCCA() {
+		return cca;
+		}
+
+}
